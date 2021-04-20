b28c6383d640dfedfd5dd0ce43e9e86795da3236
Goomba41 <anton.borodawkin@yandex.ru>
2021-04-20 11:26:22 +0300

    API: overall: update language pick function (take language from 5 different places)

--- app/__init__.py
+++ app/__init__.py
@@ -31,6 +31,29 @@ app.register_blueprint(APIv1_0_0, url_prefix='/API/v1.0.0')
 @babel.localeselector
 def get_locale():
     """Get preferred language from HTTP header."""
-    print(request.accept_languages)
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-    # return "ru"
+    language = None
+    languages = app.config['LANGUAGES']
+
+    # 5. Take language from AcceptLanguages Header or request
+    language = request.accept_languages.best_match(languages)
+
+    # 4. Take language from Cookie
+    cookie_parameter = request.cookies.get('lang', None)
+    if cookie_parameter and cookie_parameter in languages:
+        language = cookie_parameter
+
+    # 3. Take language from DB (from user settings)
+    # Make later after user edit
+
+    # 2. Take language from domain
+    host = request.host
+    domain_parse_list = host.split('.')
+    if domain_parse_list[0] in languages:
+        language = domain_parse_list[0]
+
+    # 1. Take language from request argument &lang=
+    request_parameter = request.args.get('lang', None)
+    if request_parameter and request_parameter in languages:
+        language = request_parameter
+
+    return language
